6b0663fb979bbd85ee08ec2f67e94e272ea603ce
gustavodsantos <gustavojuniordos@hotmail.com>
2025-03-14 22:49:40 -0300

    criado página inicial

---
+++ frontend/vuetify/src/components/Cursos.vue
@@ -0,0 +1,46 @@
+<script setup>
+  import { useCursosStore } from '@/stores/cursos';
+  import { onMounted, ref } from 'vue';
+
+  const store = useCursosStore();
+  const { cursos } = store;
+  const loading = ref(true);
+  const error = ref(null);
+
+onMounted(async () => {
+  console.log('Componente montado. Buscando cursos...');  // Verifique no console
+  try {
+    await store.fetchCursos();
+  } catch (error) {
+    error.value = 'Erro ao carregar cursos. Tente novamente mais tarde.';
+  } finally {
+    loading.value = false;
+  }
+});
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row v-if="cursos.length > 0">
+          <v-col v-for="curso in cursos" :key="curso.id" class="border rounded-lg" cols="12" md="4">
+            <v-card>
+              <v-card-title>{{ curso.titulo }}</v-card-title>
+              <v-card-text>{{ curso.descricao }}</v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row v-else>
+          <v-col cols="12">
+            <v-alert type="info">Carregando cursos...</v-alert>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped lang="sass">
+
+</style>
---
+++ frontend/vuetify/src/stores/cursos.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import axios from 'axios';
+
+export const useCursosStore = defineStore('cursos', () => {
+  const cursos = ref([]);
+
+  async function fetchCursos() {
+    try {
+      const response = await axios.get('http://localhost:8000/api/cursos/');
+      console.log('Dados recebidos:', response.data);  // Verifique os dados no console
+      cursos.value = response.data;  // Atualiza o estado da store
+      console.log('Cursos na store:', cursos.value);  // Verifique se os dados foram atualizados
+    } catch (error) {
+      console.error('Erro ao buscar cursos:', error);
+    }
+  }
+
+  return { cursos, fetchCursos };
+});
